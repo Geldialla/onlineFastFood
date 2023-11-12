5b517e579efef4cf10d0f48132c1ab16b8dddd03
Your Name <you@example.com>
2023-11-12 03:59:16 +0100

    creating DB and Config it

---
+++ src/app/services/Data-Layer/db-config.ts
@@ -0,0 +1,16 @@
+import { LocalDbConfig } from './geldi-be-mock.modue';
+
+export const dbConfig: LocalDbConfig[] = [
+    {
+      table: "Product",
+      columns: ['id', 'image', 'title', 'name', 'category', 'description', 'price']
+    },
+    {
+      table: "Order",
+      columns: ['id', 'userId', 'productId', 'productName', 'productPrice']
+    },
+    {
+      table: "Category",
+      columns: ['id', 'productId',]
+    },
+  ];
